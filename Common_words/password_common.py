8fdb905370b98941e7c30f9c73e53c0fe9c6106c
vbb08 <67701977+vbb08@users.noreply.github.com>
2023-03-03 16:20:56 +0100

    Update password_common.py
    Creation of app where is possible to type in a text in a textbox area, and print it out, in the same page, after pressing the submit button

--- Common_words/password_common.py
+++ Common_words/password_common.py
@@ -1,68 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
 import random
 
 app = Flask(__name__)
-
-#to make appear something on browser
-#@app.route("/")
-#def home():
-#    return "Hello World!"
-
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-@app.route('/pass/', methods=['GET'])
-def show_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    random_letters = random.sample(letters, 7)
-    random.shuffle(random_letters)
-    password = ''
-    password = password.join(random_letters)
-    show_password = password
-    return render_template('index.html', show_password = show_password)
-
-###
-
-@app.route('/bla/', methods=['GET'])
-def testing():
-    fh=open('words.txt')
-    for line in fh:
-        line=line.rstrip()
-        print(line)
-    return ('index.html', line)
-
 ###################################
+'''
+#to show the test of the .txt file
+@app.route('/apri/', methods=['GET'])
+def request():
+    fname = input("Enter file name: ")
+    fh=open(fname,'r')
+    return render_template("index.html", apri=fh)
+'''
 
-@app.route('/commonwords/', methods=['GET'])
+
+@app.route('/commonwords/', methods=['GET', 'POST'])
 def commonwords():
-    fh=open('words.txt','r')
-    dic=dict()
-    for line in fh:
-        line=line.rstrip()
-        words=line.split()
-        for word in words:
-            dic[word]=dic.get(word,0)+1
-#print(dic)
+    '''
 
-    lst=list()
-    for k,v in dic.items():
-        newtup=(v,k)
-        lst.append(newtup)
-#print("flipped", lst)
+    fname = input("Enter file name: ")
+    try:
+        fh=open(fname,'r')
+    except:
+        print('Wrong file name. Insert a valid file name:')
+        quit()
+'''
 
-    lst=sorted(lst, reverse=True)
-#print("sorted", lst[:10])
+    if request.method == 'POST':
+#        title = request.form['title']
+        content = request.form['content']
+        fh=content
+        fh=list()
+        fh.append({'content': content})
+#        fh.append({'title': title, 'content': content})
+#        return redirect(url_for('index'))
 
-    for v,k in lst[:10]:
-        print(k,v)
+        dic=dict()
+#        fh=str()
+        for line in fh:
+            print(line)
 
-    return render_template('index.html', k, v)
+    return render_template('index.html' , commonwords = line)
 
 
 if __name__ == "__main__":
